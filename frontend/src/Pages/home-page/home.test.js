4bfddfd9b329e7e765a86099d4d9bda74a2b4aca
Alexandra Codreanu <ioana.codreanu@student.unitbv.ro>
2025-01-26 21:56:35 +0200

    added 2 unit tests for home page login form

--- frontend/src/Pages/home-page/home.test.js
+++ frontend/src/Pages/home-page/home.test.js
@@ -1,10 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import axios from "axios";
 
 import Home from "./home";
-
-jest.mock("axios");
 
 test("renders email input", () => {
   render(
@@ -16,4 +13,39 @@ test("renders email input", () => {
     "example_username / example@mail.com"
   );
   expect(emailInput).toBeInTheDocument();
+});
+
+test("updates email input value on change", () => {
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+  const emailInput = screen.getByPlaceholderText(
+    "example_username / example@mail.com"
+  );
+  fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+  expect(emailInput.value).toBe("test@example.com");
+});
+
+test("render password error", async () => {
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+
+  const emailInput = screen.getByPlaceholderText(
+    "example_username / example@mail.com"
+  );
+
+  fireEvent.change(emailInput, { target: { value: "test" } });
+
+  const loginButton = screen.getByRole("button", { name: /log in/i });
+  fireEvent.click(loginButton);
+
+  await waitFor(() => {
+    const passwordError = screen.getByText("Please enter a password");
+    expect(passwordError).toBeInTheDocument();
+  });
 });
