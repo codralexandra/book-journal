483f5d2500e887c0e0b313567782e313c1b29a8a
Alexandra Codreanu <ioana.codreanu@student.unitbv.ro>
2025-01-26 23:16:32 +0200

    fixed chat scroll + styling

--- frontend/src/Components/book-search-component/BookSearch.js
+++ frontend/src/Components/book-search-component/BookSearch.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Button } from "react-bootstrap";
+
 import defaultCover from "../../assets/default-cover.jpg";
 
 const BookSearch = ({ books, loading }) => {
